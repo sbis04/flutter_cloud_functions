69b9b8aa54fd7c7b82d2521554d03fe809a47bb3
Souvik Biswas <sbis1999@gmail.com>
2022-02-12 20:27:53 +0530

    Update index file

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,24 @@
 const functions = require("firebase-functions");
+const sgMail = require("@sendgrid/mail");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+sgMail.setApiKey(functions.config().sendgrid.key);
 
+const msg = {
+  to: "souvik_bt18@iiitkalyani.ac.in", // Change to your recipient
+  from: "sbis1999@gmail.com", // Change to your verified sender
+  subject: "Welcome to your account",
+  text: "This is your first email triggered by Cloud Functions",
+};
 
+exports.sendEmailToUser = functions.https.onRequest((req, res) => {
+  sgMail
+      .send(msg)
+      .then((response) => {
+        console.log(response[0].statusCode);
+        console.log(response[0].headers);
+      })
+      .catch((error) => {
+        console.error(`Unable to send email. Error: ${error}`);
+        throw new functions.https.HttpsError("aborted", "Unable to send email");
+      });
+});
