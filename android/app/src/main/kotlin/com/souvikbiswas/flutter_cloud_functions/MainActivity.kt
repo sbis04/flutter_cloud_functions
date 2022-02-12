ea42c2b81c22a6c9e4655a609561b1c0c1dcb306
Souvik Biswas <sbis1999@gmail.com>
2022-02-12 12:52:27 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/souvikbiswas/flutter_cloud_functions/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.souvikbiswas.flutter_cloud_functions
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Cloud Functions',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cloud Functions'),
+      ),
+      body: Container(),
+    );
+  }
+}
